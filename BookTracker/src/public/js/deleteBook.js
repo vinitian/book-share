5c49394029a05852ace4784842c9c690cd17c4ec
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2026-09-25 00:52:50 +0200

    feat: delete bookcard from ui on deleteBook without having to refresh page, show "Deleting..." once delete button pressed

--- BookTracker/src/public/js/deleteBook.js
+++ BookTracker/src/public/js/deleteBook.js
@@ -1,4 +1,9 @@
 async function deleteBook(id) {
+  const bookcard = document.querySelector(`.book-card[data-book-id="${id}"]`);
+  const deleteButton = bookcard.querySelector(".delete");
+
+  deleteButton.textContent = "Deleting...";
+
   const response = await fetch("/delete", {
     method: "DELETE",
     headers: {
@@ -8,8 +13,10 @@ async function deleteBook(id) {
   });
 
   if (!response.ok) {
+    deleteButton.textContent = "Deletion failed";
     throw new Error("Failed to delete book");
   }
 
+  bookcard.remove();
   return response.json();
 }
