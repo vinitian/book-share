2b04c958fb3e95860b62804083052cf22292ab05
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2026-09-24 15:14:26 +0200

    refactor: delete unused comments

--- BookService/src/index.js
+++ BookService/src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 // const cors = require("cors");
-// const axios = require("axios");
 const BookManager = require("./bookManager");
 
 const app = express();
@@ -25,35 +24,22 @@ app.use(express.urlencoded({ extended: true }));
 // -- parse requests of content-type - application/x-www-form-urlencoded
 // app.use(express.urlencoded({ extended: true }));
 
-// app.locals.dateFns = require("date-fns");
-
 const router = express.Router();
 router.get("/", getBooks);
 router.get("/search", searchBooks);
 router.post("/add", addBook);
 app.use("/", router);
 
-// app.get("/", (req, res) => {
-//   return res.status(200).json({ hello: "world" });
-// });
-
 function getBooks(req, res) {
   const bookManager = new BookManager();
-  return (
-    bookManager
-      .connect()
-      .then(() => bookManager.getBooks())
-      // .then((result) =>
-      //   result.flat().map((r) => {
-      //     return { title: title, author: r.author };
-      //   }),
-      // )
-      .then((r) => {
-        console.log("Number of results:", r.length);
-        return r;
-      })
-      .then((cleaned) => res.send(cleaned))
-  );
+  return bookManager
+    .connect()
+    .then(() => bookManager.getBooks())
+    .then((r) => {
+      console.log("Number of results:", r.length);
+      return r;
+    })
+    .then((cleaned) => res.send(cleaned));
 }
 
 function searchBooks(req, res) {
@@ -62,30 +48,24 @@ function searchBooks(req, res) {
   let searchTerm = req.query.q.replaceAll("+", " ").trim();
   let bookManager = new BookManager();
   console.log("Searching for:", searchTerm);
-  return (
-    bookManager
-      .connect()
-      .then(() => bookManager.searchBooks(searchTerm))
-      .then((result) =>
-        result.flat().map((r) => {
-          return {
-            _id: r._id,
-            title: r.title,
-            author: r.author,
-            owner: r.owner,
-            borrower: r.borrower,
-            isBorrowed: r.isBorrowed,
-            datePosted: r.datePosted,
-            dateBorrowed: r.dateBorrowed,
-          };
-        }),
-      )
-      // .then((r) => {
-      //   console.log("Number of results:", r.length);
-      //   return r;
-      // })
-      .then((cleaned) => res.send(cleaned))
-  );
+  return bookManager
+    .connect()
+    .then(() => bookManager.searchBooks(searchTerm))
+    .then((result) =>
+      result.flat().map((r) => {
+        return {
+          _id: r._id,
+          title: r.title,
+          author: r.author,
+          owner: r.owner,
+          borrower: r.borrower,
+          isBorrowed: r.isBorrowed,
+          datePosted: r.datePosted,
+          dateBorrowed: r.dateBorrowed,
+        };
+      }),
+    )
+    .then((cleaned) => res.send(cleaned));
 }
 
 async function addBook(req, res) {
