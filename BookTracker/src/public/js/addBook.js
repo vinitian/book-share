b9a04ad2dd92098cb7b02eeb2b2f1c2b30a9eb67
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2026-09-25 00:42:06 +0200

    fix: addBook body request was not properly sent, "Book added!" text appending if 2 or more books are added in one go

--- BookTracker/src/public/js/addBook.js
+++ BookTracker/src/public/js/addBook.js
@@ -1,19 +1,24 @@
 const form = document.querySelector("form");
+const titleInput = form.querySelector(".title input");
+const authorInput = form.querySelector(".author input");
 
 form.addEventListener("submit", async (event) => {
   event.preventDefault();
+  const title = titleInput.value;
+  const author = authorInput.value;
 
   const response = await fetch("/add", {
     method: "POST",
-    body: new FormData(form),
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, author }),
   });
 
   if (!response.ok) {
     throw new Error("Failed to add book");
   }
-  const book = await response.json();
 
-  const message = document.createElement("p");
+  const message = document.querySelector("#book-added-text");
   message.textContent = "Book added!";
-  form.append(message);
 });
