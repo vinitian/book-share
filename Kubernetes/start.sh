c60e9a73cba004f2167520ce1304e438eb87fad0
Vinita Jungwiwattanaporn <vinitajwp@gmail.com>
2026-09-25 10:29:15 +0200

    feat: use default context for minikube

--- Kubernetes/start.sh
+++ Kubernetes/start.sh
@@ -2,6 +2,11 @@
 
 # cd into Kubernetes first, then run this script
 
+# Don't need to run these three lines if you're using Windows/Mac
+minikube start
+kubectl config use-context minikube
+docker context use default
+
 kubectl apply -f booktracker.yaml -l data=config
 kubectl apply -f booktracker.yaml -l app=mongodb
 
@@ -12,7 +17,5 @@ kubectl get configmap/mongodb-config -o yaml    | sed -r "s/NOTSET/$mongodb_ip/"
 kubectl apply -f booktracker.yaml -l app=booktrackerapp
 kubectl apply -f booktracker.yaml -l app=bookservice
 
-minikube start
-
-minikube service booktracker-service
-minikube service bookservice-service
+# Don't need to run this line if you're using Windows/Mac
+minikube service booktrackerapp-service
